af5ff5c0347b6e7aacec6119c727009657346e5c
mefekaskaya <mefekaskaya@gmail.com>
2023-02-21 15:58:50 +0300

    simple button test with react testing library

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+export function replaceCamelWithSpace(colorName) {
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+}
 
 function App() {
-  const [buttonColor, setButtonColor] = useState('red');
-
+  const [buttonColor, setButtonColor] = useState("MediumViolentRed");
+  const [disableButton, setDisableButton] = useState(false);
+  const newButtonColor =
+    buttonColor === "MediumViolentRed" ? "MidnightBlue" : "MediumViolentRed";
   const changeColor = () => {
-    if (buttonColor === 'red') {
-      setButtonColor('blue');
+    if (buttonColor === "MediumViolentRed") {
+      setButtonColor("MidnightBlue");
     } else {
-      setButtonColor('red')
+      setButtonColor("MediumViolentRed");
     }
-  }
+  };
+
   return (
     <div>
-      <button style={{ backgroundColor: buttonColor }} onClick={changeColor}>
-        {buttonColor === 'red' ? 'Change to Blue' : 'Change to Red'}
+      <button
+        style={{ backgroundColor: disableButton ? "gray" : buttonColor }}
+        onClick={changeColor}
+        disabled={disableButton}
+      >
+        Change to {replaceCamelWithSpace(newButtonColor)}
       </button>
+      <input
+        id="disable-button-checkbox"
+        type="checkbox"
+        checked={disableButton}
+        onChange={(e) => setDisableButton(e.target.checked)}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,56 +1,51 @@
-import { logRoles } from '@testing-library/react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+// import { logRoles } from '@testing-library/react';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App, { replaceCamelWithSpace } from "./App";
 
-// test('renders learn react', () => {
-//   render(<App />);
-//   const linkElement = screen.getByRole('link', { name: /sadass/i }); // We should use getByRole insteaad of getByText to find element because getByRole has higher prioerity. Check https://testing-library.com/docs/queries/about/#priority. By the way, getByRole takes two argument. First argument is the role we are looking for and the second argument is an object which we can define options. In this case, we  used name option which represents text inside the role we are loooking for.
-//   // const linkElement = screen.getByText(/learn React/i);
-//   expect(linkElement).toBeInTheDocument()
-// })
-
-// We can read the role whenever possible to make sure that my elements are accessible by screen readers.
-/*
-  So how do we know to look for which role?
-    Check this link: https://www.w3.org/TR/wai-aria/#role_definitions
-    This definitions of roles tell you what roles are possible. We can add a role to any element, like a div, by using role attribute. Role can be anything in that link.
-      <div className="App" role="cell">
-  Some elements are built in roles. A button, for example, has automatically a rule of button. And as we just saw, an anchor element whşch is <a>, has a role of link.
-*/
-/*
-  If we can't find an element like a screen reader would?
-    then our app isn't friendly to screen readers.
-*/
-
-test('button has correct initial color', () => {
-  const { container } = render(<App />);
-  // logRoles(container)
-  // find an element with a role of button and the text of 'Change to Blue' 
-  const buttonElement = screen.getByRole('button', { name: 'Change to Blue' });
-  // expect the background color to be red
-  expect(buttonElement).toHaveStyle({ 'background-color': 'red' })
-  // click button
-
-})
-
-// test('button has correct initial text', () => {
-
-// })
-
-test('button turns blue when clicked', () => {
+test("button has correct initial color", () => {
   render(<App />);
-  // find an element with a role of button and the text of 'Change to Blue' 
-  const buttonElement = screen.getByRole('button', { name: 'Change to Blue' });
-  // expect the background color to be red
-  fireEvent.click(buttonElement);
-  expect(buttonElement).toHaveStyle({ 'background-color': 'blue' })
-})
+  const buttonElement = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
+  expect(buttonElement).toHaveStyle({ "background-color": "MediumViolentRed" });
+});
 
-test('button text changes when clicked', () => {
+test("button turns blue when clicked", () => {
   render(<App />);
-  // find an element with a role of button and the text of 'Change to Blue' 
-  const buttonElement = screen.getByRole('button', { name: 'Change to Blue' });
-  // expect the background color to be red
+  const buttonElement = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
   fireEvent.click(buttonElement);
-  expect(buttonElement).toHaveTextContent('Change to Red')
-})
+  expect(buttonElement).toHaveStyle("background-color: MidnightBlue");
+});
+
+test("button color should be gray when checkbox is checked", () => {
+  render(<App />);
+  const buttonElement = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
+  const checkboxElement = screen.getByRole("checkbox", {
+    name: "Disable button",
+  });
+  fireEvent.click(checkboxElement);
+  expect(checkboxElement).toBeChecked();
+  expect(buttonElement).toHaveStyle({ "background-color": "gray" });
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toHaveStyle({ backgroundColor: "MediumViolentRed" });
+});
+
+test("click button and then checkbox, test button color respoectively", () => {
+  render(<App />);
+  const buttonElement = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
+  const checkboxElement = screen.getByRole("checkbox", {
+    name: "Disable button",
+  });
+  fireEvent.click(buttonElement);
+  expect(buttonElement).toHaveStyle({ backgroundColor: "MidnightBlue" });
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toHaveStyle({ "background-color": "gray" });
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toHaveStyle({ backgroundColor: "MidnightBlue" });
+});
